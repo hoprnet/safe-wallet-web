e2d0c6b2a67a3ed44cfaff3d4694e6d6b1f2443d
Tino Breddin <t@tolbrino.com>
2023-07-14 13:59:46 +0200

    Fix docker image to use runtime environment variables

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -1,6 +1,6 @@
 import chains from './chains'
 
-export const IS_PRODUCTION = process.env.NEXT_PUBLIC_IS_PRODUCTION
+export const IS_PRODUCTION = process.env.NEXT_PUBLIC_IS_PRODUCTION === 'true'
 
 export const GATEWAY_URL_PRODUCTION =
   process.env.NEXT_PUBLIC_GATEWAY_URL_PRODUCTION || 'https://safe-client.safe.global'
